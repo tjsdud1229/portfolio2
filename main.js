01e2a7cd9395acddcee356b6b433e9422cfda598
tjsdud1229 <98548308+tjsdud1229@users.noreply.github.com>
2022-02-02 13:05:39 -0800

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strick';
 
-//Make navbar transparent when it is on the top//
+//Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', ()=> {
@@ -8,7 +8,21 @@ document.addEventListener('scroll', ()=> {
     navbar.classList.add('navbar--dark');
   } else {
     navbar.classList.remove('navbar--dark');
-  
+  }
+})
+
+// Handle scrolling when tapping on the navbar menu 
+
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event)=>{
+  const target = event.target;
+  const link = target.dataset.link;
+  if(link == null) {
+    return
   }
 
-})
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior: 'smooth'});
+
+});
